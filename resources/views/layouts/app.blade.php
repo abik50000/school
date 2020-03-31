d48074028cccbca0f604fa592c3959f4616568a0
abik50000 <abik50000@gmail.com>
2020-03-31 12:55:45 +0600

    c

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,10 +52,12 @@
             @yield('content')
         </div>
         
-
-        @include('layouts.footers.auth')
-
+        @auth()
+            @include('layouts.footers.auth')
+        @endauth
         
+
+
         @guest()
             @include('layouts.footers.guest')
         @endguest
