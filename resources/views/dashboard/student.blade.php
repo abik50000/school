f6496eca9a0ce738c4de9544b0295591545e97d5
abik50000 <abik50000@gmail.com>
2020-04-01 13:30:48 +0600

    c

--- resources/views/dashboard/student.blade.php
+++ resources/views/dashboard/student.blade.php
@@ -63,7 +63,7 @@
                     
                                         <!-- <p>Уведомления <span>{{ $notifications->count() }}</span></p> -->
                                   
-                                    <ul class="flex-column flex-md-row prof-tabs" id="tabs-icons-text" role="tablist">
+                                    <ul class="nav flex-column  prof-tabs" id="tabs-icons-text" role="tablist">
                                         <li class="prof-menu">
                                             <a class="nav-link mb-sm-3 mb-md-0 active" id="tabs-icons-text-1-tab" data-toggle="tab" href="#tabs-icons-text-1" role="tab" aria-controls="tabs-icons-text-1" aria-selected="true">
                                                 <i class="ni ni-cloud-upload-96 mr-2"></i> Сегодня 
