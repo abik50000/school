2d747a0489e66179cf00f06a18afb3ec436ed628
abik50000 <abik50000@gmail.com>
2020-04-01 10:42:09 +0600

    c

--- resources/views/dashboard/student.blade.php
+++ resources/views/dashboard/student.blade.php
@@ -47,110 +47,113 @@
         <div class="row align-items-center">
 
                             <div class="col col-xl-4 mb-5 mb-xl-0">
-                                <h6 class="text-uppercase text-light ls-1 mb-1">Сегодня</h6>
-                                <h2 class="mb-0">{{ date('d.m.Y') }}</h2>
-                                <hr>
-                                <h3 style="color: #fff"><i class="ni ni-bell-55 mr-2"></i> Уведомления ({{ $notifications->count() }})</h3>
-                                @foreach($notifications as $note)
-                                    <p class="notification"><span></span> {{ $note->text }}</p>
-                                @endforeach
+                                <div class="white-block">
+                                    <h6 class="text-uppercase text-light ls-1 mb-1">Сегодня</h6>
+                                    <h2 class="mb-0">{{ date('d.m.Y') }}</h2>
+                                    <hr>
+                                    <h3 style="color: #fff"><i class="ni ni-bell-55 mr-2"></i> Уведомления ({{ $notifications->count() }})</h3>
+                                    @foreach($notifications as $note)
+                                        <p class="notification"><span></span> {{ $note->text }}</p>
+                                    @endforeach
+                                </div>
                             </div> 
                             <div class="col col-xl-8 mb-5 mb-xl-0">
-                                <div class="nav-wrapper">
-                                    <ul class="nav nav-pills nav-fill flex-column flex-md-row" id="tabs-icons-text" role="tablist">
-                                        <li class="nav-item">
-                                            <a class="nav-link mb-sm-3 mb-md-0 active" id="tabs-icons-text-1-tab" data-toggle="tab" href="#tabs-icons-text-1" role="tab" aria-controls="tabs-icons-text-1" aria-selected="true"><i class="ni ni-cloud-upload-96 mr-2"></i> Сегодня <span class="badge badge-success-2">{{ $timetables->count() }}</span></a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link mb-sm-3 mb-md-0" id="tabs-icons-text-2-tab" data-toggle="tab" href="#tabs-icons-text-2" role="tab" aria-controls="tabs-icons-text-2" aria-selected="false"><i class="ni ni-bell-55 mr-2"></i> Оценки</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link mb-sm-3 mb-md-0" id="tabs-icons-text-3-tab" data-toggle="tab" href="#tabs-icons-text-3" role="tab" aria-controls="tabs-icons-text-3" aria-selected="false"><i class="ni ni-calendar-grid-58 mr-2"></i>Расписание</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="card shadow">
-                                    <div class="_card-body">
-                                        <div class="tab-content" id="myTabContent">
-                                            <div class="tab-pane fade show active" id="tabs-icons-text-1" role="tabpanel" aria-labelledby="tabs-icons-text-1-tab">
-                                                <table class="table align-items-center white-bg">
-                                                    <thead class="thead-light">
-                                                        <tr>
-                                                           <th>#</th>
-                                                           <th>Время</th>
-                                                           <th>Предмет</th>
-                                                           <th>Кабинет</th>
-                                                           <th class="text-right">Преподаватель</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        @foreach($timetables as $timetable)
-                                                        <tr>
-                                                            <th scope="row">{{ $loop->iteration }}</th>
-                                                            <td><i class="bg-warning"></i>{{ $timetable->lesson->start }} - {{ $timetable->lesson->end }}</td>
-                                                            <td>
-                                                                <span class="badge badge-dot mr-4"><i class="bg-success"></i>
-                                                                   {{ $timetable->subject->name }}
-                                                                </span>
-                                                            </td>
-                                                            <td>{{ $timetable->cabinet->cabinet }}</td>
-                                                            <td class="text-right">{{ $timetable->teacher->firstname }} {{ $timetable->teacher->lastname }}</td>
-                                                        </tr>
-                                                        @endforeach
-                                                      </tbody>
-                                                </table>
-                                            </div>
-                                            <div class="tab-pane fade" id="tabs-icons-text-2" role="tabpanel" aria-labelledby="tabs-icons-text-2-tab">
-                                                <div class="_card-body">
-                                                    <div class="">
-                                                        <table class="table align-items-center">
-                                                        @foreach($subjects as $subject)
-                                                        
-                                                            
-                                                                <tr>
-                                                                    <td><h3 style="margin-bottom: 0;">{{ $subject->name }}</h3></td>
-                                                                    <td style="width: 100%;">
-                                                                        @foreach($subject->marks as $mark_wrap)
-                                                                            @foreach($mark_wrap as $mark)
-                                                                                <button class="btn btn-default mark-btn" type="button" data-id="{{ $mark->mark->id }}">{{ $mark->mark->mark }}</button>
-                                                                            @endforeach
-                                                                        @endforeach
-                                                                    </td>
-                                                                </tr>
-                                                           
+                                <div class="white-block">
+                                    <div class="nav-wrapper">
+                                        <ul class="nav nav-pills nav-fill flex-column flex-md-row" id="tabs-icons-text" role="tablist">
+                                            <li class="nav-item">
+                                                <a class="nav-link mb-sm-3 mb-md-0 active" id="tabs-icons-text-1-tab" data-toggle="tab" href="#tabs-icons-text-1" role="tab" aria-controls="tabs-icons-text-1" aria-selected="true"><i class="ni ni-cloud-upload-96 mr-2"></i> Сегодня <span class="badge badge-success-2">{{ $timetables->count() }}</span></a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link mb-sm-3 mb-md-0" id="tabs-icons-text-2-tab" data-toggle="tab" href="#tabs-icons-text-2" role="tab" aria-controls="tabs-icons-text-2" aria-selected="false"><i class="ni ni-bell-55 mr-2"></i> Оценки</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link mb-sm-3 mb-md-0" id="tabs-icons-text-3-tab" data-toggle="tab" href="#tabs-icons-text-3" role="tab" aria-controls="tabs-icons-text-3" aria-selected="false"><i class="ni ni-calendar-grid-58 mr-2"></i>Расписание</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="card shadow">
+                                        <div class="_card-body">
+                                            <div class="tab-content" id="myTabContent">
+                                                <div class="tab-pane fade show active" id="tabs-icons-text-1" role="tabpanel" aria-labelledby="tabs-icons-text-1-tab">
+                                                    <table class="table align-items-center white-bg">
+                                                        <thead class="thead-light">
+                                                            <tr>
+                                                               <th>#</th>
+                                                               <th>Время</th>
+                                                               <th>Предмет</th>
+                                                               <th>Кабинет</th>
+                                                               <th class="text-right">Преподаватель</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            @foreach($timetables as $timetable)
+                                                            <tr>
+                                                                <th scope="row">{{ $loop->iteration }}</th>
+                                                                <td><i class="bg-warning"></i>{{ $timetable->lesson->start }} - {{ $timetable->lesson->end }}</td>
+                                                                <td>
+                                                                    <span class="badge badge-dot mr-4"><i class="bg-success"></i>
+                                                                       {{ $timetable->subject->name }}
+                                                                    </span>
+                                                                </td>
+                                                                <td>{{ $timetable->cabinet->cabinet }}</td>
+                                                                <td class="text-right">{{ $timetable->teacher->firstname }} {{ $timetable->teacher->lastname }}</td>
+                                                            </tr>
+                                                            @endforeach
+                                                          </tbody>
+                                                    </table>
+                                                </div>
+                                                <div class="tab-pane fade" id="tabs-icons-text-2" role="tabpanel" aria-labelledby="tabs-icons-text-2-tab">
+                                                    <div class="_card-body">
+                                                        <div class="">
+                                                            <table class="table align-items-center">
+                                                            @foreach($subjects as $subject)
                                                             
                                                                 
+                                                                    <tr>
+                                                                        <td><h3 style="margin-bottom: 0;">{{ $subject->name }}</h3></td>
+                                                                        <td style="width: 100%;">
+                                                                            @foreach($subject->marks as $mark_wrap)
+                                                                                @foreach($mark_wrap as $mark)
+                                                                                    <button class="btn btn-default mark-btn" type="button" data-id="{{ $mark->mark->id }}">{{ $mark->mark->mark }}</button>
+                                                                                @endforeach
+                                                                            @endforeach
+                                                                        </td>
+                                                                    </tr>
+                                                               
+                                                                
+                                                                    
+                                                                
+                                                                 
                                                             
-                                                             
-                                                        
-                                                        @endforeach
-                                                        </table>
+                                                            @endforeach
+                                                            </table>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                            <div class="tab-pane fade" id="tabs-icons-text-3" role="tabpanel" aria-labelledby="tabs-icons-text-3-tab">
-                                                <div class="card-body">
-                                                    <h3>Текущая неделя</h3>
-                                                    <button class="btn btn-default" type="button">Понедельник <br> 20 января</button>
-                                                    <button class="btn btn-default" type="button">Вторник <br> 21 января</button>
-                                                    <button class="btn btn-default" type="button">Среда <br> 22 января</button>
-                                                    <button class="btn btn-default" type="button">Четверг <br> 23 января</button>
-                                                    <button class="btn btn-default" type="button">Пятница <br> 24 января</button>
-                                                    <button class="btn btn-default" type="button">Суббота <br> 25 января</button>
+                                                <div class="tab-pane fade" id="tabs-icons-text-3" role="tabpanel" aria-labelledby="tabs-icons-text-3-tab">
+                                                    <div class="card-body">
+                                                        <h3>Текущая неделя</h3>
+                                                        <button class="btn btn-default" type="button">Понедельник <br> 20 января</button>
+                                                        <button class="btn btn-default" type="button">Вторник <br> 21 января</button>
+                                                        <button class="btn btn-default" type="button">Среда <br> 22 января</button>
+                                                        <button class="btn btn-default" type="button">Четверг <br> 23 января</button>
+                                                        <button class="btn btn-default" type="button">Пятница <br> 24 января</button>
+                                                        <button class="btn btn-default" type="button">Суббота <br> 25 января</button>
 
-                                                    <h3 class="mt-5">Следующая неделя</h3>
-                                                    <button class="btn btn-default" type="button">Понедельник <br> 27 января</button>
-                                                    <button class="btn btn-default" type="button">Вторник<br> 28 января</button>
-                                                    <button class="btn btn-success" type="button">Среда<br> 29 января</button>
-                                                    <button class="btn btn-secondary" type="button">Четверг<br> 30 января</button>
-                                                    <button class="btn btn-secondary" type="button">Пятница<br> 31 января</button>
-                                                    <button class="btn btn-secondary" type="button">Суббота<br> 1 февраля</button>
+                                                        <h3 class="mt-5">Следующая неделя</h3>
+                                                        <button class="btn btn-default" type="button">Понедельник <br> 27 января</button>
+                                                        <button class="btn btn-default" type="button">Вторник<br> 28 января</button>
+                                                        <button class="btn btn-success" type="button">Среда<br> 29 января</button>
+                                                        <button class="btn btn-secondary" type="button">Четверг<br> 30 января</button>
+                                                        <button class="btn btn-secondary" type="button">Пятница<br> 31 января</button>
+                                                        <button class="btn btn-secondary" type="button">Суббота<br> 1 февраля</button>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                
                             </div>
                         </div>
                    
